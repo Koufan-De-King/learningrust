e566f93e775ccbc5071a44e8d94d0eddf7773b90
Koufan-De-King <kingkoufan@gmail.com>
2024-06-12 07:39:01 +0100

    (backend): fixed commit issues

--- src/fib.rs
+++ src/fib.rs
@@ -1,7 +1,7 @@
 pub fn fibo(number: u64) -> u64 {
     if number < 2 {
-       return number
-    } else {
-        return fibo(number-1) + fibo(number-2)
+       number
+    }else { 
+       fibo(number - 1) + fibo(number - 2) 
     }
 }
--- src/temperature.rs
+++ src/temperature.rs
@@ -1,5 +1,5 @@
 // formula: °C = (°F - 32) × 5/9
 pub fn conv(celsius: f32) -> f32 {
-    let fahrenheit: f32 = ((celsius*9.0/5.0) + 32.0) as f32;
+    let fahrenheit: f32 = ((celsius * 9.0 / 5.0) + 32.0) as f32;
     fahrenheit
 }
